461128aadc2db5b1e024430d28bb855f54152569
Joshua Esclamado <113429401+codingJoshua123@users.noreply.github.com>
2026-04-04 03:23:06 +0000

    commit

--- OOP_Project.java
+++ OOP_Project.java
@@ -1,5 +1,19 @@
+/**
+ * Codespace creator: 
+ * @author Joshua M. Esclamado
+ * 
+ * Date created: 
+ * 04/04/2026
+ * 
+ * Co-authors: 
+ * 
+ * 
+ * Program description: 
+ * This codespace is prepared to set the environment for the OOP project written in Java language.
+ */
+
 public class OOP_Project {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
     }
 }
