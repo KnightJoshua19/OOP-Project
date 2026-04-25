bf42d161c14c7d77ffad1392762fb8ea7990696d
jooecoodes <jefeljoevillacorta@gmail.com>
2026-04-25 15:11:58 +0800

    Add check in system initial logic

--- OOP_Project.java
+++ OOP_Project.java
@@ -1,19 +1,30 @@
+
 /**
- * Codespace creator: 
+ * Codespace creator:
+ * 
  * @author Joshua M. Esclamado
  * 
- * Date created: 
- * 04/04/2026
+ *         Date created:
+ *         04/04/2026
  * 
- * Co-authors: 
+ *         Co-authors:
+ *         Jefel Joe V. Villacorta
  * 
  * 
- * Program description: 
- * This codespace is prepared to set the environment for the OOP project written in Java language.
+ *         Program description:
+ *         This codespace is prepared to set the environment for the OOP project
+ *         written in Java language.
  */
+
+import hotel.CheckInSystem;
 
 public class OOP_Project {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        CheckInSystem hotelService = new CheckInSystem();
+
+        System.out.println("--- Starting Hotel Operations ---");
+        hotelService.processCheckIn();
+
     }
 }
